7d0f2ba6eb555e4348443897772194c38a943e15
tphill <tphill760@gmail.com>
2023-10-13 09:58:36 -0400

    final push

--- menu-app/app/components/logo.js
+++ menu-app/app/components/logo.js
@@ -2,6 +2,8 @@ export default function Logo(){
     return(
         <div className="logo">
         <h1>Fig + Flora</h1>
+        <p>Mon-Sat</p>
+        <p>9a to 2p</p>
         <p>348 E Main St, Lexington, KY</p>
         </div>
     )
